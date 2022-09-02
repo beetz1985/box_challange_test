e5e1145e71068795ababd91e50b8f340962a56d9
beetz1985 <benjamintoulson@hotmail.co.uk>
2022-09-02 01:27:40 +0100

    Test of the box challange involving setting state in a parent componenet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box from './Box';
+import boxes from './boxes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [boxData, setBoxData] = React.useState(boxes);
+    
+    console.log("App Render")
+
+    function handleToggle(id) {
+        setBoxData((s)=>{
+            const boxArr = [...s];
+            const indx = boxArr.findIndex(v=>v.id === id);
+            boxArr[indx].on = (boxArr[indx].on) ? false : true;
+            return boxArr;
+        })
+    }
+
+
+    const boxElements = boxData.map((v)=>{
+        return <Box key={v.id} id={v.id} on={v.on} toggle={handleToggle}/>
+    })
+
+
+    return (
+        <div className="box-container">
+            {boxElements}
+        </div>
+    )
 }
 
 export default App;
